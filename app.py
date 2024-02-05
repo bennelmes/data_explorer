733e00497fc7e346c251bee2d725da5256da2f46
Ben Nelmes <ben@newautomotive.org>
2024-02-05 20:40:40 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import plotly_express as px
+from helpers import download_latest, co2_ppm_df
+
+download_latest(dir='assets/')
+df = co2_ppm_df(dir='assets/')
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    # html.Div(children='Hello World & Ben'),
+    # dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+    # dcc.Graph(figure=px.line(df, x='date', y=['average', 'deseasonalized'])),
+    dcc.Graph(id='graph-content'),
+    dcc.Dropdown(df.type_group.unique().tolist(), 'Actual values', id='dropdown-selection'),
+
+])
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.type_group==value]#.groupby(['date', 'variable']).sum().reset_index().pivot(index='date', values='value',  columns='variable').reset_index()
+    fig = px.line(
+        dff, 
+        x='date', 
+        y='value', 
+        color='variable',
+        labels={'average': 'Average monthly value'}
+        ).update_layout(
+            xaxis_title='Date',
+            legend={'title': 'Measure'},
+            title='Atmospheric CO2 concentrations'
+        )
+    if value == 'Actual values':
+        fig.update_layout(yaxis_title='CO2 parts per million')
+    else:
+        fig.update_layout(yaxis_title='Year-on-year change in CO2 PPM')
+    return fig
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ helpers.py
@@ -0,0 +1,22 @@
+import requests
+import pandas as pd
+
+
+def download_latest(dir='data/'):
+
+        r = requests.get('https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_mm_mlo.csv')
+        with open(dir + 'co2_mm_mlo.csv', 'wb') as f:
+                f.write(r.content)
+
+def co2_ppm_df(dir='data/'):
+        # df = pd.read_csv(dir+'co2_mm_mlo.csv', encoding='utf-8', comment='#')
+        df = pd.read_csv('https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_mm_mlo.csv', comment='#')
+        df['date'] = df.apply(lambda row: pd.to_datetime(str(int(row.year)) + '-' + str(int(row.month)), format='%Y-%m'), axis=1)
+        df = df[['date', 'average', 'deseasonalized']]
+        # df['average_year_change'] = df.average.diff(12)
+        df['deseasonalized_change'] = df.deseasonalized.diff(12)
+        df = df[df.date > '1959-12-31']
+        df = df.melt(id_vars='date')
+        df['type_group'] = df.apply(lambda row: 'Actual values' if row.variable in ['average', 'deseasonalized'] else 'Annual Change', axis=1)
+        # df['variable'] = df['variable'].apply(lambda x: x.replace('average_year_change', 'average').replace('deseasonalized_change', 'deseasonalized'))
+        return df
