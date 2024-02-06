647661941fc86be983af341511742a0644000f6e
bennelmes <ben@newautomotive.org>
2024-02-06 14:53:44 +0000

    major upgrade

--- app_ecc.py
+++ app_ecc.py
@@ -1,22 +1,23 @@
 import dash
 from dash import Dash, html, dcc
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__, use_pages=True)
+app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
 
 app.layout = html.Div([
-    html.H1('Multi-page app with Dash Pages'),
-    html.Div([
-        html.Div(
-            dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
-        ) for page in dash.page_registry.values()
-    ]),
+    # html.H1('Multi-page app with Dash Pages'),
+    # html.Div([
+    #     html.Div(
+    #         dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
+    #     ) for page in dash.page_registry.values()
+    # ]),
     dash.page_container
 ])
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
 
 # from dash import Dash, html, dash_table, dcc, callback, Output, Input
